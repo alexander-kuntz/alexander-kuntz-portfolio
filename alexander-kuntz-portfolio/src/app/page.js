607e863f1d27b1bcddc17f15e390081defe0e026
Alex Kuntz <alexanderaak@hotmail.com>
2025-02-22 16:11:54 -0600

    Colouring

--- alexander-kuntz-portfolio/src/app/page.js
+++ alexander-kuntz-portfolio/src/app/page.js
@@ -1,28 +1,28 @@
-export default function Home() {
+export default function Home() { // bg-slate-900    |    cyan-900      |         text-cyan-300    | Border: border-4 border-cyan-300
   return (
-    <div className="bg-gray-50 py-24 sm:py-32">
+    <div className="bg-gradient-to-b from-slate-900 to-cyan-900 py-24 sm:py-32">
       <div className="mx-auto max-w-2xl px-6 lg:max-w-7xl lg:px-8">
-        <img className="rounded-full w-40 h-50 mx-auto" src="/Kuntz-Profile-Photo.jpeg" alt="image description"></img>
-        <p className="mx-auto mt-2 max-w-lg text-center text-4xl font-semibold tracking-tight text-balance text-gray-950 sm:text-5xl">
+        <img className="rounded-full w-40 h-50 mx-auto border-4 border-cyan-300" src="/Kuntz-Profile-Photo.jpeg" alt="image description"></img>
+        <p className="mx-auto mt-2 max-w-lg text-center text-4xl font-semibold tracking-tight text-balance text-cyan-50 sm:text-5xl">
           Alexander Kuntz
         </p>
-        <h2 className="text-center text-base/7 font-semibold text-indigo-600">Software Developer</h2>
+        <h2 className="text-center text-base/7 font-semibold text-cyan-300">Junior Software Developer</h2>
         <div className="mt-10 grid gap-4 sm:mt-16 lg:grid-cols-3 lg:grid-rows-2">
           <div className="relative lg:row-span-2">
-            <div className="absolute inset-px rounded-lg bg-white lg:rounded-l-[2rem]"></div>
+            <div className="absolute inset-px rounded-lg bg-slate-900 lg:rounded-l-[2rem] border-4 border-cyan-300"></div>
             <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(var(--radius-lg)+1px)] lg:rounded-l-[calc(2rem+1px)]">
               <div className="px-8 pt-8 pb-3 sm:px-10 sm:pt-10 sm:pb-0">
-                <p className="mt-2 text-lg font-medium tracking-tight text-gray-950 max-lg:text-center">
+                <p className="mt-2 text-lg font-medium tracking-tight text-cyan-200 max-lg:text-center">
                   Professional Projects
                 </p>
-                <p className="mt-2 max-w-lg text-sm/6 text-gray-600 max-lg:text-center">
+                <p className="mt-2 max-w-lg text-sm/6 text-cyan-100  max-lg:text-center">
                   Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.
                 </p>
               </div>
               <div className="@container relative min-h-[30rem] w-full grow max-lg:mx-auto max-lg:max-w-sm">
-                <div className="absolute inset-x-10 top-10 bottom-0 overflow-hidden rounded-t-[12cqw] border-x-[3cqw] border-t-[3cqw] border-gray-700 bg-gray-900 shadow-2xl">
+                <div className="absolute inset-x-10 top-10 bottom-10 rounded-t-[12cqw]   border-gray-700 bg-gray-900 ">
                   <img
-                    className="size-full object-cover object-top"
+                    className="size-80 object-cover object-top"
                     src="https://tailwindui.com/plus-assets/img/component-images/bento-03-mobile-friendly.png"
                     alt=""
                   />
@@ -32,13 +32,13 @@ export default function Home() {
             <div className="pointer-events-none absolute inset-px rounded-lg ring-1 shadow-sm ring-black/5 lg:rounded-l-[2rem]"></div>
           </div>
           <div className="relative max-lg:row-start-1">
-            <div className="absolute inset-px rounded-lg bg-white max-lg:rounded-t-[2rem]"></div>
+            <div className="absolute inset-px rounded-lg bg-slate-900 max-lg:rounded-t-[2rem] border-4 border-cyan-300"></div>
             <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(var(--radius-lg)+1px)] max-lg:rounded-t-[calc(2rem+1px)]">
               <div className="px-8 pt-8 sm:px-10 sm:pt-10">
-                <p className="mt-2 text-lg font-medium tracking-tight text-gray-950 max-lg:text-center">
+                <p className="mt-2 text-lg font-medium tracking-tight text-cyan-200 max-lg:text-center">
                    Personal Projects
                   </p>
-                <p className="mt-2 max-w-lg text-sm/6 text-gray-600 max-lg:text-center">
+                <p className="mt-2 max-w-lg text-sm/6 text-cyan-100 max-lg:text-center">
                   Lorem ipsum, dolor sit amet consectetur adipisicing elit maiores impedit.
                 </p>
               </div>
@@ -50,14 +50,14 @@ export default function Home() {
                 />
               </div>
             </div>
-            <div className="pointer-events-none absolute inset-px rounded-lg ring-1 shadow-sm ring-black/5 max-lg:rounded-t-[2rem]"></div>
+            <div className="pointer-events-none absolute inset-px rounded-lg ring-1 shadow-sm ring-black/5 max-lg:rounded-t-[2rem] border-4 border-cyan-300"></div>
           </div>
           <div className="relative max-lg:row-start-3 lg:col-start-2 lg:row-start-2">
-            <div className="absolute inset-px rounded-lg bg-white"></div>
+            <div className="absolute inset-px rounded-lg bg-slate-900 border-4 border-cyan-300"></div>
             <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(var(--radius-lg)+1px)]">
               <div className="px-8 pt-8 sm:px-10 sm:pt-10">
-                <p className="mt-2 text-lg font-medium tracking-tight text-gray-950 max-lg:text-center">Security</p>
-                <p className="mt-2 max-w-lg text-sm/6 text-gray-600 max-lg:text-center">
+                <p className="mt-2 text-lg font-medium tracking-tight text-cyan-200 max-lg:text-center">Security</p>
+                <p className="mt-2 max-w-lg text-sm/6 text-cyan-100 max-lg:text-center">
                   Morbi viverra dui mi arcu sed. Tellus semper adipiscing suspendisse semper morbi.
                 </p>
               </div>
@@ -72,29 +72,17 @@ export default function Home() {
             <div className="pointer-events-none absolute inset-px rounded-lg ring-1 shadow-sm ring-black/5"></div>
           </div>
           <div className="relative lg:row-span-2">
-            <div className="absolute inset-px rounded-lg bg-white max-lg:rounded-b-[2rem] lg:rounded-r-[2rem]"></div>
+            <div className="absolute inset-px rounded-lg bg-slate-900 max-lg:rounded-b-[2rem] lg:rounded-r-[2rem] border-4 border-cyan-300"></div>
             <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(var(--radius-lg)+1px)] max-lg:rounded-b-[calc(2rem+1px)] lg:rounded-r-[calc(2rem+1px)]">
               <div className="px-8 pt-8 pb-3 sm:px-10 sm:pt-10 sm:pb-0">
-                <p className="mt-2 text-lg font-medium tracking-tight text-gray-950 max-lg:text-center">
+                <p className="mt-2 text-lg font-medium tracking-tight text-cyan-200 max-lg:text-center">
                   Collaborative Projects
                 </p>
-                <p className="mt-2 max-w-lg text-sm/6 text-gray-600 max-lg:text-center">
+                <p className="mt-2 max-w-lg text-sm/6 text-cyan-100 max-lg:text-center">
                   Sit quis amet rutrum tellus ullamcorper ultricies libero dolor eget sem sodales gravida.
                 </p>
               </div>
-              <div className="relative min-h-[30rem] w-full grow">
-                <div className="absolute top-10 right-0 bottom-0 left-10 overflow-hidden rounded-tl-xl bg-gray-900 shadow-2xl">
-                  <div className="flex bg-gray-800/40 ring-1 ring-white/5">
-                    <div className="-mb-px flex text-sm/6 font-medium text-gray-400">
-                      <div className="border-r border-b border-r-white/10 border-b-white/20 bg-white/5 px-4 py-2 text-white">
-                        NotificationSetting.jsx
-                      </div>
-                      <div className="border-r border-gray-600/10 px-4 py-2">App.jsx</div>
-                    </div>
-                  </div>
-                  <div className="px-6 pt-6 pb-14">{/* Your code example */}</div>
-                </div>
-              </div>
+              
             </div>
             <div className="pointer-events-none absolute inset-px rounded-lg ring-1 shadow-sm ring-black/5 max-lg:rounded-b-[2rem] lg:rounded-r-[2rem]"></div>
           </div>
